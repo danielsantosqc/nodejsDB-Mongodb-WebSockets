d95a97f0858799819e1fcec63fa4380312c05492
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-06-14 23:48:06 -0300

    add network-message.js, It is in charge of receiving the request from the client, treating it according to the type of method it is, it receives the information from the ruta.js file |  Se encarga de recibir la petición del cliente, tratándola según que tipo de método sea, recibe la información del fichero rutas.js

---
+++ components/message/network-message.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const response = require('../../network/response');
+const controller = require('./controller');
+const router = express.Router();
+
+//rotess.............
+router.get('/', function (req, res) {
+  console.log(req.headers);
+  res.header({
+    "custom-header": "Nuestro valor personalizado",
+  });
+
+  response.success(req, res, 'Lista de mensajes')
+  console.log("message from GET");
+})
+
+router.post('/', function (req, res) {
+
+  controller.addMessage(req.body.user, req.body.message)
+    .then((fullMessage) => {
+      response.success(req, res, fullMessage, 201);
+    })
+    .catch(e => {
+      response.error(req, res, 'Informacion Invalida', 400, ' Error en el controller');
+    });
+  console.log("message from POST");
+});
+
+router.delete('/', function (req, res) {
+  console.log(req.body);
+  console.log(req.query);
+  // res.send("message delet");
+  response.success(req, res, 'message DELETE ok doki...')
+
+  console.log("delete........ rom DELETE");
+})
+
+
+
+module.exports = router;
